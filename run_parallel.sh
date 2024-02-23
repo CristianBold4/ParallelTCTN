9a2417021d6aee6705f13625a1944ea01b9d113e
CristianBold4 <boldrincristian00@gmail.com>
2024-02-23 18:35:38 +0100

    added run script

---
+++ run_parallel.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+
+./build/Tonic @ 
+# ./build/Tonic 4177 100000 0.05 0.2 /mnt/datasets/stackoverflow_preprocessed.txt /mnt/out_stackoverflow /mnt/oracles/oracle_stackoverflow_top10.txt 
